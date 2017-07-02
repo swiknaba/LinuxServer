35106c819fb732e63d88feb12b7e2d9ea6e6c064
swiknaba <ludwig.reinmiedl@gmail.com>
2017-07-02 17:23:45 +0200

    final touch

--- db_setup.py
+++ db_setup.py
@@ -38,8 +38,7 @@ class Item(Base):
     slug = Column(String(80), nullable=False)
     description = Column(String(250))
     categorie_id = Column(Integer, ForeignKey('categorie.id'))
-    categorie = relationship(Categorie, cascade='all, delete-orphan',
-                             single_parent=True)
+    categorie = relationship(Categorie)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
